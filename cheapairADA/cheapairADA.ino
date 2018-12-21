733b68a51dd0cf7c1f72724d250940781e8c896e
Daniel Herrero <daniel.herrero@kaleidos.net>
2018-12-21 11:29:07 +0100

    Fix webserver

--- cheapairADA/cheapairADA.ino
+++ cheapairADA/cheapairADA.ino
@@ -13,6 +13,15 @@
 #include <WiFi101.h>
 #include <WiFiMDNSResponder.h>
 
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "KSCHOOL-ALUMNOS";        // your network SSID (name)
+char pass[] = "kschool-2017";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+
 // OLED related
 
 
@@ -169,6 +178,114 @@ void ledO3output(int ppmO3Value){
    shutdownLed(ledO3);
   }
 }
+
+
+
+
+
+
+
+
+// MQ-131 section
+
+/****************** MQResistanceCalculation ****************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/
+float MQResistanceCalculation(int raw_adc)
+{
+  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+}
+
+/***************************** MQCalibration ****************************************
+Input:   sensorO3NO2 - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use
+         MQResistanceCalculation to calculates the sensor resistance in clean air
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about
+         10, which differs slightly between different sensors.
+************************************************************************************/
+float MQCalibration(int mq_pin)
+{
+  int i;
+  float val=0;
+
+  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+    val += MQResistanceCalculation(analogRead(mq_pin));
+    delay(CALIBRATION_SAMPLE_INTERVAL);
+  }
+  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro
+  return val;                                                      //according to the chart in the datasheet
+
+}
+
+/*****************************  MQRead *********************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/
+float MQRead(int mq_pin)
+{
+  int i;
+  float rs=0;
+
+  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    rs += MQResistanceCalculation(analogRead(mq_pin));
+    delay(READ_SAMPLE_INTERVAL);
+  }
+
+  rs = rs/READ_SAMPLE_TIMES;
+
+  return rs;
+}
+
+/*****************************  MQGetGasPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function passes different curves to the MQGetPercentage function which
+         calculates the ppm (parts per million) of the target gas.
+************************************************************************************/
+long MQGetGasPercentage(float rs_ro_ratio, int gas_id)
+{
+  if ( gas_id == GAS_NOx ) {
+     return MQGetPercentage(rs_ro_ratio,NOxCurve);
+  } else if ( gas_id == GAS_O3 ) {
+     return MQGetPercentage(rs_ro_ratio,O3Curve);
+
+  }
+
+  return 0;
+}
+
+/*****************************  MQGetPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         pcurve      - pointer to the curve of the target gas
+Output:  ppm of the target gas
+Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
+         of the line could be derived if y(rs_ro_ratio) is provided. As it is a
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
+         value.
+************************************************************************************/
+long  MQGetPercentage(float rs_ro_ratio, float *pcurve)
+{
+  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+}
+
+
+
+
+
+
+
+
 //------------------------------------------------
 
 // WIFI SERVER
@@ -179,7 +296,7 @@ char mdnsName[] = "cheapair"; // the MDNS name that the board will respond to
 // the name above, so "wifi101" will be accessible on
 // the MDNS name "wifi101.local".
 
-WiFiServer server(80);
+//WiFiServer server(80);
 
 // Create a MDNS responder to listen and respond to MDNS name requests.
 WiFiMDNSResponder mdnsResponder;
@@ -220,22 +337,22 @@ void WIFISetup(){
     
 }
 
-void printWiFiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
-}
+//void printWiFiStatus() {
+//  // print the SSID of the network you're attached to:
+//  Serial.print("SSID: ");
+//  Serial.println(WiFi.SSID());
+//
+//  // print your WiFi shield's IP address:
+//  IPAddress ip = WiFi.localIP();
+//  Serial.print("IP Address: ");
+//  Serial.println(ip);
+//
+//  // print the received signal strength:
+//  long rssi = WiFi.RSSI();
+//  Serial.print("signal strength (RSSI):");
+//  Serial.print(rssi);
+//  Serial.println(" dBm");
+//}
 
 void WebServer(WiFiClient client, int co, int no2, int o3, int g){
 
@@ -341,9 +458,63 @@ void ledGoutput(int u, int v, int w){
 }
 
 
+void setupWifi() {
+  //Configure pins for Adafruit ATWINC1500 Feather
+  WiFi.setPins(8,7,4,2);
+  
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  server.begin();
+  // you're connected now, so print out the status:
+  printWiFiStatus();
+}
+
+
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
 // SETUP
 
 void setup() {
+
+  setupWifi();
+  
   // put your setup code here, to run once:
   timer = millis() + 1000;
   Serial.println(timer);
@@ -381,8 +552,72 @@ void setup() {
   Serial.print(Ro);
   Serial.println("kohm");
   display.clearDisplay();
-  
+}
 
+void publishOnWebServer(float co, float no2, float o3, float g) {
+    // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          client.print("<body>");
+
+          client.print("CO = ");
+          client.print(co);
+          client.print(" ppm");
+          client.print("<br/>");
+
+          client.print("NO2 = ");
+          client.print(no2);
+          client.print(" ppm");
+          client.print("<br/>");
+
+          client.print("O3 = ");
+          client.print(o3);
+          client.print(" ppm");
+          client.print("<br/>");
+
+          client.print("G = ");
+          client.println(g);
+          client.print(" ppm");
+          client.print("<br/>");
+          client.print("</body>");
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
 
 
@@ -390,7 +625,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 
   float co = readCO();
 //  int no2 = readNO2();
@@ -400,19 +634,21 @@ void loop() {
   int g = co + ch4 + o3;
 //
 //  int co = 1000;
-//  int no2 = 1000;
+  float no2 = co;
 //  int o3 = 1000;
 //  int g = 1000;
 
+  publishOnWebServer(co, no2, o3, g);
 
- if( (long)(millis()-timer) >= 0) {
-    ledCOoutput(co);
-    ledCH4output(ch4);
-    ledO3output(o3);
-    ledGoutput(co,ch4,o3);
 
-    timer += 1000;
-  }
+// if( (long)(millis()-timer) >= 0) {
+//    ledCOoutput(co);
+//    ledCH4output(ch4);
+//    ledO3output(o3);
+//    ledGoutput(co,ch4,o3);
+//
+//    timer += 1000;
+//  }
 
   Serial.print("CO = ");
   Serial.print(co);
@@ -422,119 +658,20 @@ void loop() {
   Serial.print(o3);
   Serial.print("\t G = ");
   Serial.println(g);
-
-  display.setCursor(0,0);
-  display.print("CO ppm:");
-  display.println(co);
-  display.print("CH4 ppm:");
-  display.println(ch4);
-  display.print("O3 ppm:");
-  display.println(o3);
-  display.print("General:");
-  display.println(g);
-  display.setCursor(0,0);
-  display.display(); // actually display all of the above
-  delay(10);
-   display.clearDisplay();
-  yield();
-  delay(1000);
-}
-
-
-
-
-
-
-
-// MQ-131 section
-
-/****************** MQResistanceCalculation ****************************************
-Input:   raw_adc - raw value read from adc, which represents the voltage
-Output:  the calculated sensor resistance
-Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
-         across the load resistor and its resistance, the resistance of the sensor
-         could be derived.
-************************************************************************************/
-float MQResistanceCalculation(int raw_adc)
-{
-  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
-}
-
-/***************************** MQCalibration ****************************************
-Input:   sensorO3NO2 - analog channel
-Output:  Ro of the sensor
-Remarks: This function assumes that the sensor is in clean air. It use
-         MQResistanceCalculation to calculates the sensor resistance in clean air
-         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about
-         10, which differs slightly between different sensors.
-************************************************************************************/
-float MQCalibration(int mq_pin)
-{
-  int i;
-  float val=0;
-
-  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
-    val += MQResistanceCalculation(analogRead(mq_pin));
-    delay(CALIBRATION_SAMPLE_INTERVAL);
-  }
-  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
-  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro
-  return val;                                                      //according to the chart in the datasheet
-
-}
-
-/*****************************  MQRead *********************************************
-Input:   mq_pin - analog channel
-Output:  Rs of the sensor
-Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
-         The Rs changes as the sensor is in the different consentration of the target
-         gas. The sample times and the time interval between samples could be configured
-         by changing the definition of the macros.
-************************************************************************************/
-float MQRead(int mq_pin)
-{
-  int i;
-  float rs=0;
-
-  for (i=0;i<READ_SAMPLE_TIMES;i++) {
-    rs += MQResistanceCalculation(analogRead(mq_pin));
-    delay(READ_SAMPLE_INTERVAL);
-  }
-
-  rs = rs/READ_SAMPLE_TIMES;
-
-  return rs;
-}
-
-/*****************************  MQGetGasPercentage **********************************
-Input:   rs_ro_ratio - Rs divided by Ro
-         gas_id      - target gas type
-Output:  ppm of the target gas
-Remarks: This function passes different curves to the MQGetPercentage function which
-         calculates the ppm (parts per million) of the target gas.
-************************************************************************************/
-long MQGetGasPercentage(float rs_ro_ratio, int gas_id)
-{
-  if ( gas_id == GAS_NOx ) {
-     return MQGetPercentage(rs_ro_ratio,NOxCurve);
-  } else if ( gas_id == GAS_O3 ) {
-     return MQGetPercentage(rs_ro_ratio,O3Curve);
-
-  }
-
-  return 0;
-}
-
-/*****************************  MQGetPercentage **********************************
-Input:   rs_ro_ratio - Rs divided by Ro
-         pcurve      - pointer to the curve of the target gas
-Output:  ppm of the target gas
-Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
-         of the line could be derived if y(rs_ro_ratio) is provided. As it is a
-         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
-         value.
-************************************************************************************/
-long  MQGetPercentage(float rs_ro_ratio, float *pcurve)
-{
-  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+//
+//  display.setCursor(0,0);
+//  display.print("CO ppm:");
+//  display.println(co);
+//  display.print("CH4 ppm:");
+//  display.println(ch4);
+//  display.print("O3 ppm:");
+//  display.println(o3);
+//  display.print("General:");
+//  display.println(g);
+//  display.setCursor(0,0);
+//  display.display(); // actually display all of the above
+//  delay(10);
+//   display.clearDisplay();
+//  yield();
+//  delay(1000);
 }
